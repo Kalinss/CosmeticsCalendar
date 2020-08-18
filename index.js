50d66f4519aaaa0ede7c81b8641509e65fc2265e
Kalinss <Kalinss16@gmail.com>
2020-08-18 23:45:33 +0400

    Updates

--- index.js
+++ index.js
@@ -212,7 +212,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "192.168.0.104" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "42210" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50025" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
